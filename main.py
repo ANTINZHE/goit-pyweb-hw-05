71afd63e773c2b5403102c9402c2bb60a547749d
ANTIN <antonzhelihovskiy@gmail.com>
2025-10-06 03:04:00 +0300

    complete a base code

---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+import platform
+import aiohttp
+import asyncio
+from datetime import date, timedelta, datetime
+
+
+def get_currency_rates(result):
+    """Фільтруємо потрібні валюти"""
+    rates = result.get("exchangeRate")
+    filtered = [r for r in rates if r.get("currency") in ("USD", "EUR")]
+    return filtered
+
+
+async def get_normal_url(session, date):
+    """Отримуємо дані з API"""
+    url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={date}'
+
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                print(f"Error {response.status}")
+                return {}
+    except aiohttp.ClientError as e:
+        print(e)
+        return {}
+
+
+def get_dats_from_sys():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    try:
+        days = int(sys.argv[1])
+        if 1 <= days <= 10:
+            return days
+        else:
+            print(" < 10")
+            sys.exit(1)
+    except ValueError:
+        sys.exit(1)
+
+async def process_day(session, str_date):
+    """Отримуємо дані для однієї дати"""
+    result = await get_normal_url(session, str_date)
+    currency_rates = get_currency_rates(result)
+    return {
+        str_date: {
+            rate["currency"]: {
+                "sale": rate.get("saleRate"),
+                "purchase": rate.get("purchaseRate")
+            }
+            for rate in currency_rates
+        }
+    }
+
+
+async def main():
+    today_date = date.today()
+    count_of_days = get_dats_from_sys()
+    main_list = []
+
+    async with aiohttp.ClientSession() as session:
+        for day in range(count_of_days):
+            current_date = today_date - timedelta(days=day)
+            str_date = datetime.strftime(current_date, "%d.%m.%Y")
+            day_data = await process_day(session, str_date)
+            main_list.append(day_data)
+    return main_list
+
+if __name__ == "__main__":
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    r = asyncio.run(main())
+    print(r)
